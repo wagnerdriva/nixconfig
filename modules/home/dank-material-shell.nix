2b358e8c06aef8ca67d7c1fb2888a18f742cbcee
Wagner <wagner@datadriva.com>
2026-09-16 19:19:01 -0300

    feat: add transparency to DMS bar

--- modules/home/dank-material-shell.nix
+++ modules/home/dank-material-shell.nix
@@ -82,6 +82,76 @@ in
       showWorkspaceApps = true;
       maxWorkspaceIcons = 4;
       workspaceActiveAppHighlightEnabled = true;
+
+      # Let the wallpaper show through the bar while keeping its contents
+      # fully opaque for readability.
+      barConfigs = [{
+        id = "default";
+        name = "Main Bar";
+        enabled = true;
+        position = 0;
+        screenPreferences = [ "all" ];
+        showOnLastDisplay = true;
+        leftWidgets = [ "launcherButton" "workspaceSwitcher" "focusedWindow" ];
+        centerWidgets = [ "music" "clock" "weather" ];
+        rightWidgets = [
+          "systemTray"
+          "clipboard"
+          "cpuUsage"
+          "memUsage"
+          "notificationButton"
+          "battery"
+          "controlCenterButton"
+        ];
+        spacing = 4;
+        innerPadding = 4;
+        barInsetPadding = -1;
+        barLengthPadding = 0;
+        bottomGap = 0;
+        attachToScreenEdge = false;
+        transparency = 0.78;
+        widgetTransparency = 1.0;
+        squareCorners = false;
+        noBackground = false;
+        maximizeWidgetIcons = false;
+        maximizeWidgetText = false;
+        removeWidgetPadding = false;
+        widgetPadding = 8;
+        batteryColorMode = "theme";
+        gothCornersEnabled = false;
+        gothCornerRadiusOverride = false;
+        gothCornerRadiusValue = 12;
+        borderEnabled = false;
+        borderColor = "surfaceText";
+        borderOpacity = 1.0;
+        borderThickness = 1;
+        widgetOutlineEnabled = false;
+        widgetOutlineColor = "primary";
+        widgetOutlineOpacity = 1.0;
+        widgetOutlineThickness = 1;
+        fontScale = 1.0;
+        iconScale = 1.0;
+        autoHide = false;
+        autoHideStrict = false;
+        autoHideDelay = 250;
+        showOnWindowsOpen = false;
+        openOnOverview = false;
+        visible = true;
+        popupGapsAuto = true;
+        popupGapsManual = 4;
+        maximizeDetection = true;
+        useOverlayLayer = false;
+        scrollEnabled = true;
+        scrollXBehavior = "column";
+        scrollYBehavior = "workspace";
+        shadowIntensity = 0;
+        shadowOpacity = 60;
+        shadowColorMode = "default";
+        shadowCustomColor = "#000000";
+        clickThrough = false;
+        hoverPopouts = false;
+        hoverPopoutDelay = 150;
+      }];
     };
   };
 
